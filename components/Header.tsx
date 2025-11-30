2ddf11ad73c7df338e41cedb89a14e7be2426073
Andres Hung <andres.hung@outlook.com>
2025-11-30 11:39:52 -0600

    Increase text size

--- components/Header.tsx
+++ components/Header.tsx
@@ -13,7 +13,7 @@ export default function Header() {
   return (
     <header className="w-full flex flex-col sm:flex-row items-center px-4 py-3 gap-4 bg-ub-purple">
       {/* UB FormFlow Logo */}
-      <div className="text-xl font-bold whitespace-nowrap flex items-center gap-2 shrink-0">
+      <div className="text-2xl font-bold whitespace-nowrap flex items-center gap-2 shrink-0">
         <Link
           href="/"
           className="text-ub-yellow flex items-center gap-2 shrink-0"
