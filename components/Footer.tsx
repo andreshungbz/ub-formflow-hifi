851b4341192d409d64ec5452a93ead93f3c99f67
Andres Hung <andres.hung@outlook.com>
2025-11-30 11:38:03 -0600

    Fix mail and phone links

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,13 +6,13 @@ export default function Footer() {
     <footer className="w-full flex flex-col sm:flex-row items-center justify-end px-4 py-3 gap-4 bg-ub-purple text-white">
       {/* Contact Links */}
       <Link
-        href="mailto:support@ubformflow.com"
+        href="mailto:records@ub.edu.bz"
         className="flex items-center gap-2 hover:text-ub-yellow transition-colors"
       >
         <Mail size={20} /> records@ub.edu.bz
       </Link>
       <Link
-        href="tel:+1234567890"
+        href="tel:+5012231234"
         className="flex items-center gap-2 hover:text-ub-yellow transition-colors"
       >
         <Phone size={20} /> +501 223-1234
